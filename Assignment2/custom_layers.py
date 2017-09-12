a645d631ed48278763979e730a7c760ce0a56e75
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-09-12 22:29:30 +0530

    Assignment 2 code ready

--- Assignment2/custom_layers.py
+++ Assignment2/custom_layers.py
@@ -6,37 +6,34 @@ import numpy as np
 
 class Dropout(Layer):
 
-	def __init__(self, output_dim, **kwargs):
-		self.output_dim = output_dim
-		super(Dropout, self).__init__(**kwargs)
+    def __init__(self, probability, **kwargs):
+        self.probability = probability
+        assert(probability>=0 and probability<=1)
+        super(Dropout, self).__init__(**kwargs)
 
-	def build(self, input_shape):
-		self.kernel = self.add_weight(name='kernel', 
-									  shape=(input_shape[1], self.output_dim),
-									  initializer='uniform',
-									  trainable=True)
-		super(Dropout, self).build(input_shape)
+    def build(self, input_shape):
+        super(Dropout, self).build(input_shape)
 
-	def call(self, x):
-		return K.dot(x, self.kernel)
+    def call(self, x):
+        return K.in_train_phase(K.dropout(x, self.probability),x)
 
-	def compute_output_shape(self, input_shape):
-		return (input_shape[0], self.output_dim)
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 
 class BatchNorm(Layer):
 
-	def __init__(self, output_dim, **kwargs):
-		self.epsilon = 0.0001
-		super(BatchNorm, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        self.epsilon = K.epsilon()
+        super(BatchNorm, self).__init__(**kwargs)
 
-	def build(self, input_shape):
-		super(BatchNorm, self).build(input_shape)
+    def build(self, input_shape):
+        super(BatchNorm, self).build(input_shape)
 
-	def call(self, x):
-		mean = K.mean(x, axis=0)
-		variance = K.var(x, axis=0)
-		return (x - mean) / K.sqrt(variance + self.epsilon)
+    def call(self, x):
+        mean = K.mean(x, axis=0)
+        variance = K.var(x, axis=0)
+        return K.in_train_phase((x - mean) / K.sqrt(variance + self.epsilon),x)
 
-	def compute_output_shape(self, input_shape):
-		return input_shape
+    def compute_output_shape(self, input_shape):
+        return input_shape
