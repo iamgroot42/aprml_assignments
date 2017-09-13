656c65706cc1443d792aba400b3954be5cd89e66
Anshuman Suri <iamgroot42@users.noreply.github.com>
2017-09-13 17:05:23 +0000

    Update custom_layers.py

--- Assignment2/custom_layers.py
+++ Assignment2/custom_layers.py
@@ -6,34 +6,34 @@ import numpy as np
 
 class Dropout(Layer):
 
-    def __init__(self, probability, **kwargs):
-        self.probability = probability
-        assert(probability>=0 and probability<=1)
-        super(Dropout, self).__init__(**kwargs)
+	def __init__(self, probability, **kwargs):
+		self.probability = probability
+		assert(probability>=0 and probability<=1)
+		super(Dropout, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        super(Dropout, self).build(input_shape)
+	def build(self, input_shape):
+		super(Dropout, self).build(input_shape)
 
-    def call(self, x):
-        return K.in_train_phase(K.dropout(x, self.probability),x)
+	def call(self, x):
+		return K.in_train_phase(K.dropout(x, self.probability),x)
 
-    def compute_output_shape(self, input_shape):
-        return input_shape
+	def compute_output_shape(self, input_shape):
+		return input_shape
 
 
 class BatchNorm(Layer):
 
-    def __init__(self, **kwargs):
-        self.epsilon = K.epsilon()
-        super(BatchNorm, self).__init__(**kwargs)
+	def __init__(self, **kwargs):
+		self.epsilon = K.epsilon()
+		super(BatchNorm, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        super(BatchNorm, self).build(input_shape)
+	def build(self, input_shape):
+		super(BatchNorm, self).build(input_shape)
 
-    def call(self, x):
-        mean = K.mean(x, axis=0)
-        variance = K.var(x, axis=0)
-        return K.in_train_phase((x - mean) / K.sqrt(variance + self.epsilon),x)
+	def call(self, x):
+		mean = K.mean(x, axis=0)
+		variance = K.var(x, axis=0)
+		return K.in_train_phase((x - mean) / K.sqrt(variance + self.epsilon),x)
 
-    def compute_output_shape(self, input_shape):
-        return input_shape
+	def compute_output_shape(self, input_shape):
+		return input_shape
